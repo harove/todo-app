939f282c8d9276ade4840a2cc82f6ce1a59d4709
edgar <harove@gmail.com>
2019-06-04 11:23:19 -0400

    manual CRUD implemented

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,49 @@
 class TodosController < ApplicationController
+   before_action :find_todo, only: [:show, :edit, :update, :destroy]
+
+   def index
+       @todo = Todo.all
+   end
+
+   def new
+       @todo = Todo.new
+   end
+
+   def create
+       @todo = Todo.new(todo_params)
+       @todo.save
+       redirect_to todos_path
+   end
+
+   def show
+      
+   end
+
+   def edit
+      
+   end
+
+   def update
+      
+       @todo.update(todo_params)
+       redirect_to todos_path
+   end
+
+   def destroy
+      
+       @todo.destroy
+       redirect_to todos_path
+   end
+
+private
+   def find_todo
+       @todo = Todo.find(params[:id])
+   end
+
+   def todo_params
+       params.require(:todo).permit(:description, :completed)
+   end 
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'todos', to: 'todos#index'
+  get 'todos/new', as: 'new_todo'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
 end
